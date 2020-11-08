8af9318de64fea332684f1c179dfc326fdb3930e
Tanya <tanya.gupta20114251@gmail.com>
2020-11-08 07:01:21 +0530

    Update try2.java

--- try2.java
+++ try2.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-//new comment to test git 
+//new comment to test git , it works
 public class try2 extends Application{
 	 Button newGame,existingGame,exit;
 	 Scene startMenu,startNewGame,savedGames;
